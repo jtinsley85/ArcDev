495823591a8f09e0663ccdb00067e9d67e8a69e7
Josh Tinsley <jtinsley85@outlook.com>
2020-10-24 00:11:53 -0500

    Modifixed App.tsx and added Header.tsx

---
+++ src/Components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './UI/Header';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Header/> Hello!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/UI/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//Components
+import { AppBar, Toolbar } from '@material-ui/core'
+
+// type HeaderProps = {
+//   //
+// };
+
+const Header: React.FC = () => {
+  return (
+    <AppBar>
+      <Toolbar>Arc Development</Toolbar>
+    </AppBar>
+  )
+};
+
+export default Header;
