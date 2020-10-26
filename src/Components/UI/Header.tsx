2fd3f2e36c1b2772a8d265eadb8e37fd51627043
Josh Tinsley <jtinsley85@outlook.com>
2020-10-26 11:52:06 -0500

    Implemented Elevation Scroll & Logo

--- src/Components/UI/Header.tsx
+++ src/Components/UI/Header.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+//Material-UI
+import { AppBar, Toolbar, useScrollTrigger, makeStyles } from '@material-ui/core'
 //Components
-import { AppBar, Toolbar } from '@material-ui/core'
+import logo from '../../assets/logo.svg';
 
-// type HeaderProps = {
-//   //
-// };
+//TYPES
+interface HeaderProps {
+  children: React.ReactElement;
+}
 
+//SCROLL BEHAVIOR
+const ElevationScroll = (props: HeaderProps) => {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 5 : 0,
+  });
+}
+
+//STYLES
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: '3em'
+  },
+  logo: {
+    height: '7em'
+  }
+}));
+
+
+//COMPONENT
 const Header: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <AppBar>
-      <Toolbar>Arc Development</Toolbar>
-    </AppBar>
+    <Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar disableGutters>
+            <img className={classes.logo} src={logo} alt="company logo" />
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </Fragment>
   )
 };
 
