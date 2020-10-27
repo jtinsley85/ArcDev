428393c4adde3a7814930b3208ef383a89dc874c
Josh Tinsley <jtinsley85@outlook.com>
2020-10-27 01:14:02 -0500

    Imported React Router Dom package and setup base routes in App.tsx

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //Components
 import Header from './UI/Header';
 import theme from './UI/Theme';
 //Styles
-import {ThemeProvider} from '@material-ui/core/styles'
-
+import { ThemeProvider } from '@material-ui/core/styles'
 
 
 const App: React.FC = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello!
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>}/>
+          <Route exact path="/services" component={() => <div>Services</div>}/>
+          <Route exact path="/customsoftware" component={() => <div>Custom Software</div>}/>
+          <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>}/>
+          <Route exact path="/websites" component={() => <div>Websites</div>}/>
+          <Route exact path="/revolution" component={() => <div>The Revolution</div>}/>
+          <Route exact path="/about" component={() => <div>About Us</div>}/>
+          <Route exact path="/contact" component={() => <div>Contact Us</div>}/>
+          <Route exact path="/estimate" component={() => <div>Estimate</div>}/>
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
