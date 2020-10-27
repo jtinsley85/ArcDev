00a67e5ebb097159eacc93c0a42a17c78f1473ee
Josh Tinsley <jtinsley85@outlook.com>
2020-10-27 00:56:21 -0500

    Implemented tabs for navigation and free estimate button. Still need to wire tabs up.

--- src/Components/UI/Header.tsx
+++ src/Components/UI/Header.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, ChangeEvent } from 'react';
 //Material-UI
-import { AppBar, Toolbar, useScrollTrigger, makeStyles } from '@material-ui/core'
+import { AppBar, Toolbar, useScrollTrigger, makeStyles, Tabs, Tab, Button } from '@material-ui/core'
 //Components
 import logo from '../../assets/logo.svg';
 
@@ -9,15 +9,13 @@ interface HeaderProps {
   children: React.ReactElement;
 }
 
-//SCROLL BEHAVIOR
+//SETUP SCROLL BEHAVIOR
 const ElevationScroll = (props: HeaderProps) => {
   const { children } = props;
-
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
   });
-
   return React.cloneElement(children, {
     elevation: trigger ? 5 : 0,
   });
@@ -31,6 +29,21 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: '7em'
+  },
+  tabContainer: {
+    marginLeft: 'auto'
+  },
+  tab: {
+    ...theme.typography.headerTab,
+    minWidth: 10,
+    marginLeft: '25px'
+  },
+  button: {
+    ...theme.typography.headerBtn,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    minWidth: '120px'
   }
 }));
 
@@ -39,12 +52,34 @@ const useStyles = makeStyles(theme => ({
 const Header: React.FC = () => {
   const classes = useStyles();
 
+  //SETUP TABS
+  const [tabValue, setTabValue] = useState(0);
+  const handleTabChange = (event: ChangeEvent<{}>, newValue: number) => {
+    setTabValue(newValue);
+  };
+
   return (
     <Fragment>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
             <img className={classes.logo} src={logo} alt="company logo" />
+            <Tabs
+              className={classes.tabContainer}
+              value={tabValue}
+              onChange={handleTabChange}
+              aria-label="navigation tabs"
+              indicatorColor="primary"
+            >
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
+            <Button variant='contained' color='secondary' className={classes.button}>
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
